2dbcd666401af28b7ede48147cc2cb33abfcd149
chingu-dev <aideneverage@gmail.com>
2025-05-10 21:42:04 -0400

    changed contact text

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Contact() {
   return (
     <section id="contact" className="contact card">
-      <h2><i className="fas fa-envelope"></i> Contact Information</h2>
+      <h2><i className="fas fa-envelope"></i> Contact Me</h2>
       <div className="contact-links">
         <a href="mailto:aideneverage@ufl.edu" className="contact-link">
           <i className="fas fa-envelope"></i> Email
